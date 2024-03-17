dd345d4728ad016b3180f65a22b89f8e0dbf11a4
Davide Aimar <davide.aimar99@gmail.com>
2024-03-17 18:36:31 +0100

    fix: SEO problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tamangox3.it',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
