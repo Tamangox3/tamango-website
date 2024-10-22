2182f6ccc0d910c5fcb666ed53e82f47fd8d27c0
Davide Aimar <davide.aimar99@gmail.com>
2024-10-22 23:26:17 +0200

    mockup

---
+++ src/lib/lottie.ts
@@ -0,0 +1,15 @@
+import lottie from 'lottie-web';
+
+export function loadLottieAnimation(containerId: string, animationPath: string): void {
+  const animationContainer = document.getElementById(containerId);
+  console.log('animationContainer', animationContainer);
+  if (animationContainer) {
+    lottie.loadAnimation({
+      container: animationContainer,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: animationPath
+    });
+  }
+}
