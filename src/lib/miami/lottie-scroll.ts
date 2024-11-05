b5d22b0dccd8dd36e65f4cdeafff525875e8c370
Davide Aimar <davide.aimar99@gmail.com>
2024-11-05 23:12:28 +0100

    better audio sync

--- src/lib/miami/lottie-scroll.ts
+++ src/lib/miami/lottie-scroll.ts
@@ -1,4 +1,4 @@
-import { gsap } from "gsap";
+import { gsap, Linear } from "gsap";
 import { Howl } from "howler";
 import lottie from "lottie-web";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -6,7 +6,7 @@ import ScrollToPlugin from "gsap/ScrollToPlugin";
 import type { AnimationItem } from "lottie-web";
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollToPlugin); 
 
 
 interface ExtendedAnimationItem extends AnimationItem {
@@ -42,7 +42,7 @@ export class LottieScrollTrigger {
   constructor(vars: LottieScrollTriggerVars) {
     this.logger = vars.logger;
     [
-      this.containerTarget, 
+      this.containerTarget,
       this.animationTarget
     ] = gsap.utils.toArray([vars.containerTarget, vars.animationTarget]) as HTMLElement[];
 
@@ -51,7 +51,7 @@ export class LottieScrollTrigger {
         src: [vars.audioBlob],
         format: ["mp3"],
         volume: 1,
-        html5: true,
+        html5: false,
         loop: false,
       });
     }
@@ -66,8 +66,8 @@ export class LottieScrollTrigger {
       autoplay: false,
       rendererSettings: vars.rendererSettings ?? {},
     });
-    
-    const scrollTrigger = {  
+
+    const scrollTrigger = {
       trigger: this.containerTarget,
       start: "top top",
       end: `+=${this.durationPx}px bottom`,
@@ -76,7 +76,7 @@ export class LottieScrollTrigger {
       scrub: .11,
       ...vars.scrollTriggerVars
     } satisfies ScrollTrigger.Vars;
-    
+
     this.gsapCtx = gsap.context && gsap.context();
 
     const frameAnimation = gsap.timeline({ scrollTrigger });
@@ -85,23 +85,27 @@ export class LottieScrollTrigger {
     let createTween = () => {
       this.animation.goToAndStop(playhead.frame, true);
       frameAnimation.to(playhead, {
-          frame: this.animation.totalFrames - 1,
-          ease: "none",
-          duration: frameAnimation.duration() || 1,
-          onUpdate: () => {
+        frame: this.animation.totalFrames - 1,
+        ease: "none",
+        duration: frameAnimation.duration() || 1,
+        onUpdate: () => {
+          if (this.isPaused) {
             this.animation.goToAndStop(playhead.frame, true);
-            // if audio and animation are not in sync, this is where you'd update the audio
-            if (this.audioLayer) {
-              const progress = playhead.frame / this.animation.totalFrames; // Between 0 and 1
-              const audioCurrentTime = this.audioLayer.seek();
-              const audioShouldBe = this.audioLayer.duration() * progress;
-              if (Math.abs(audioCurrentTime - audioShouldBe) > 0.5) {
-                this.logger?.log("Audio out of sync, seeking to", audioShouldBe);
-                this.audioLayer.seek(audioShouldBe);
-              }
+          } else {
+            this.animation.goToAndPlay(playhead.frame, true);
+          }
+          // if audio and animation are not in sync, this is where you'd update the audio
+          if (this.audioLayer) {
+            const progress = playhead.frame / this.animation.totalFrames; // Between 0 and 1
+            const audioCurrentTime = this.audioLayer.seek();
+            const audioShouldBe = this.audioLayer.duration() * progress;
+            if (Math.abs(audioCurrentTime - audioShouldBe) > 0.25) {
+              this.logger?.log("Audio out of sync, seeking to", audioShouldBe);
+              this.audioLayer.seek(audioShouldBe);
             }
-          },
-        }, 0);
+          }
+        },
+      }, 0);
       return () => this.animation.destroy && this.animation.destroy();
     };
 
@@ -109,34 +113,38 @@ export class LottieScrollTrigger {
 
     this.animation.frameAnimation = frameAnimation;
 
-    setTimeout(() => {
-      this.pause();
-    }, 100);
+    if (vars.debug) {
+      this.setupDebug();
+    }
   }
 
   autoscroll(duration: number) {
-    console.log("Autoscrolling", duration, this.durationPx);
     gsap.to(window, {
       scrollTo: {
-        y: this.durationPx,
+        y: this.durationPx - window.innerHeight,
         autoKill: true, // Stops autoscroll if the user scrolls manually
       },
+      ease: 'none', // linear
       duration,
-      onStart: () => this.logger?.log("Starting autoscroll"),
-      onComplete: () => this.logger?.log("Autoscroll complete"),
-      onUpdate: () => this.logger?.log("Autoscrolling", window.scrollY),
     });
   }
 
   toggleAutoscroll(isEnabled: boolean) {
     if (!this.audioLayer) return;
-    const remaining = this.audioLayer.duration() - this.audioLayer.seek();
-    if (isEnabled) this.autoscroll(remaining);
+    if (isEnabled) {
+      const remaining = this.audioLayer.duration() - this.audioLayer.seek();
+      this.logger?.log(`Scroll to ${this.durationPx} in ${remaining} seconds`);
+      this.autoscroll(remaining);
+    }
     else gsap.killTweensOf(window); // Stop autoscroll if toggled off
   }
 
   get isPaused() {
     return this.animation.isPaused;
+  }
+
+  get isPlaying() {
+    return !this.animation.isPaused;
   }
 
   play() {
@@ -151,6 +159,59 @@ export class LottieScrollTrigger {
     if (this.audioLayer?.playing()) this.audioLayer?.pause();
     this.toggleAutoscroll(false);
     this.logger?.log("Pausing animation");
+  }
+
+  private setupDebug() {
+    this.setupLottieDebug();
+    this.setupAudioDebug();
+  }
+
+  private setupLottieDebug() {
+    const lottieProgress = document.createElement("div");
+    lottieProgress.style.position = "fixed";
+    lottieProgress.style.bottom = "0";
+    lottieProgress.style.left = "0";
+    lottieProgress.style.zIndex = "1000";
+    lottieProgress.style.padding = "10px";
+    lottieProgress.style.backgroundColor = "rgba(0,0,0,.5)";
+    lottieProgress.style.color = "white";
+    this.animation.addEventListener("DOMLoaded", () => {
+      lottieProgress.textContent = `Lottie frame: 0 / ${this.animation.totalFrames}`;
+    });
+    this.animation.addEventListener("enterFrame", () => {
+      lottieProgress.textContent = `Lottie frame: ${this.animation.currentFrame.toFixed(0)} / ${this.animation.totalFrames}`;
+    });
+    document.body.appendChild(lottieProgress);
+  }
+
+  private setupAudioDebug() {
+    if (!this.audioLayer) return;
+    const audioProgress = document.createElement("div");
+    audioProgress.style.position = "fixed";
+    audioProgress.style.bottom = "44px";
+    audioProgress.style.left = "0";
+    audioProgress.style.zIndex = "1000";
+    audioProgress.style.padding = "10px";
+    audioProgress.style.backgroundColor = "rgba(0,0,0,.5)";
+    audioProgress.style.color = "white";
+    const serializedAudio = (audio: Howl) => `Audio${audio.playing() ? '' : ' not'} playing: ${audio.seek().toFixed(0)}s / ${audio.duration().toFixed(0)}s`;
+    ["play", "end", "pause", "stop", "mute", "volume", "rate", "seek", "fade", "unlock", "load", "loaderror", "playerror"].forEach(evt => {
+      this.audioLayer?.on(evt, () => {
+        let updateRef: any = undefined;
+        const onAnimationFrame = () => {
+          audioProgress.textContent = serializedAudio(this.audioLayer!);
+          if (this.audioLayer?.playing()) {
+            updateRef = requestAnimationFrame(onAnimationFrame);
+          } else {
+            if (updateRef) {
+              cancelAnimationFrame(updateRef);
+            }
+          }
+        };
+        updateRef = requestAnimationFrame(onAnimationFrame);
+      });
+    });
+    document.body.appendChild(audioProgress);
   }
 }
 
